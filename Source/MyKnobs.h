c5eadf12c6faf482743a5559b91b02c7eead5cfa
Joonas Tuovinen <joonas.tuovinen@aalto.fi>
2019-03-02 15:27:28 +0200

    New knob style.

--- Source/MyKnobs.h
+++ Source/MyKnobs.h
@@ -30,17 +30,20 @@ public:
 		auto ry = centreY - radius;
 		auto rw = radius * 2.0f;
 		auto angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
-		// fill
-		g.setColour(Colours::black);
-		g.fillEllipse(rx, ry, rw, rw);
+
+		// Paint markers
 		paintMarkers(g, centreX, centreY, radius+1.f);
-		// outline
+
+		// Knob
+		paintKnob(g, centreX, centreY, radius, rotaryStartAngle, rotaryEndAngle);
+		g.setColour(Colours::white);
+		g.fillEllipse(centreX-3.f, centreY-3.f, 6.f, 6.f);
+		// Pointer
 		Path p;
-		auto pointerLength = radius * 0.33f;
+		auto pointerLength = radius * 1.f;
 		auto pointerThickness = 2.0f;
 		p.addRectangle(-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
 		p.applyTransform(AffineTransform::rotation(angle).translated(centreX, centreY));
-		// pointer
 		g.setColour(Colours::white);
 		g.fillPath(p);
 	}
@@ -52,12 +55,49 @@ public:
 			float rad = i * (M_PI / 180.f);
 			float startX = xOrig + r * cos(rad);
 			float startY = yOrig + r * -sin(rad);
-			float endX = xOrig + (r + 3.f) * cos(rad);
-			float endY = yOrig + (r + 3.f) * -sin(rad);
+			float endX = xOrig + (r + 5.f) * cos(rad);
+			float endY = yOrig + (r + 5.f) * -sin(rad);
 
 			// Draw ticks
 			if (abs(i) % 36 == 18)
-				g.drawLine(startX, startY, endX, endY, 2.f);
+				g.fillEllipse(endX - 1.4f, endY - 1.4f, 2.8f, 2.8f);
+				//g.drawLine(startX, startY, endX, endY, 2.f);
 		}
+	}
+	void paintKnob(Graphics& g, float xOrig, float yOrig, float r, float startAngle, float endAngle)
+	{
+		g.setColour(Colours::aliceblue);
+		Path p;
+		bool pathStart = true;
+		float firstX = 0.0f;
+		float firstY = 0.0f;
+		for (auto i = -55; i <= 235; ++i)
+		{
+			float rad = i * (M_PI / 180.f);
+			float startX = xOrig + r * cos(rad);
+			float startY = yOrig + r * -sin(rad);
+
+			if (pathStart)
+			{
+				p.startNewSubPath(startX, startY);
+				firstX = startX;
+				firstY = startY;
+				pathStart = false;
+			}
+			else
+			{
+				p.lineTo(startX, startY);
+			}
+
+		}
+		p.lineTo(xOrig, yOrig + 8.f);
+		p.lineTo(firstX, firstY);
+		Path roundedPath = p.createPathWithRoundedCorners(20.0f);
+		Colour myColour = findColour(0x1005700);
+		myColour = myColour.darker(.2f);
+		g.setColour(myColour);
+		g.fillPath(roundedPath);
+		g.setColour(Colours::white);
+		g.strokePath(roundedPath, PathStrokeType(1.f));
 	}
 };
